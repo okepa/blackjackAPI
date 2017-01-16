ea961161b2847d410d5a41402d706974d0f7fba0
Tech-A38 <Tech-A38@Tech-A38s-MacBook-Air.local>
2017-01-16 11:20:17 +0000

    Added rMVC for email registration

---
+++ controllers/registerController.js
@@ -0,0 +1,24 @@
+const mailer = require("../lib/email");
+
+class RegisterController {
+    static sendContactRequest(req, res){
+        try {
+            const contactRequest = new Contact(req.body);
+            mailer.sendEmail(contactRequest)
+             .then(() => {
+                 console.log ("It worked!");
+                res.redirect("/");
+            })
+            .catch(error => {
+                console.log(`It didnt work becaue %{erorr}`);
+                res.redirect("/");
+            });
+        } catch (err){
+            console.log(err);
+            res.redirect("/");
+        }
+
+    }
+}
+
+module.exports = ContactController;
