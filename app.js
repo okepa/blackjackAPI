cb676b70c680576f8662a9e073088d66df6d600c
Tech-A42 <Tech-A42@Tech-A42s-MacBook-Air.local>
2017-01-16 11:49:54 +0000

    CREATED USER-MODEL,LOGIN CONTROLLER AND UPDATED APP.JS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const routes = require("./routes/routes");
 const app = express();
 
@@ -9,6 +10,7 @@ app.set('view engine' , 'ejs');
 //Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(mongoose);
 app.use(express.static('public'));
 app.use(routes);
 
---
+++ controllers/loginController.js
@@ -0,0 +1,8 @@
+const user = require("../models/userModel");
+const hash = require("password-hash");
+const jwt = require("jsonwebtoken");
+
+class LoginController{
+    static
+
+}
