e5ecc1a92ebb21f54a3ae439fa12e03eeac46348
Tech-A38 <Tech-A38@Tech-A38s-MacBook-Air.local>
2017-01-16 12:28:58 +0000

    Made rMVC structure

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const indexController = require("../controllers/indexController");
-const registerController = require("../constrollers/registerController");
+const registerController = require("../controllers/registerController");
 
 router.get("/", indexController.showIndex);
 
