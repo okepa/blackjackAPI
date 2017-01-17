46776baa36aad41c0fafd16fa740bfaf81713f42
Tech-A42 <Tech-A42@Tech-A42s-MacBook-Air.local>
2017-01-17 10:50:17 +0000

    REMOVED NODE_MODULES

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongodb = require("mongodb");
 const mongoose = require("mongoose");
-
 const routes = require("./routes/routes");
 
 //Connecting to the mongoose database blackjackApi
