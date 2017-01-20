f5c713632898abda9a7adbf73090d3c6eb8609f6
CassF <cassandrafrimpong@gmail.com>
2017-01-20 16:12:46 +0000

    resolved more conflicts

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,53 +1,16 @@
-<<<<<<< HEAD
 const User = require("../models/user");
 const Payment = require("../lib/payment");
-
-class PaymentController {
-
-    static getCharge(req, res) {
-        Payment.createCharge()
-            .then((resolveArray) => {
-                Payment.updateDatabase(resolveArray)
-=======
-const Payment = require("../lib/payment");
-const User = require("../models/user");
 
 class PaymentController {
     static getCharge(req, res) {
         Payment.createCharge()
             .then((resolveArray) => {
-                Charge.addChargeToDatabase(resolveArray)
->>>>>>> master
+                Payment.addChargeToDatabase(resolveArray)
+
                     .then(() => {
                         res.status(201).send("Charge updated in the database");
                     })
                     .catch(err => {
-<<<<<<< HEAD
-                        res.status(400).send(err.message);
-                    });
-            })
-    }
-
-    static createPaymentCard(req, res) {
-
-        Payment.addPaymentCard(req)
-            .then(result => {
-                res.status(200).send("added payment card");
-            })
-            .catch(err => {
-                // console.log(err);
-                res.status(400).send(err);
-            });
-
-    }
-
-}
-
-module.exports = PaymentController;
-
-
-
-=======
                         res.status(401).send(err.message);
                     });
             })
@@ -64,7 +27,20 @@ module.exports = PaymentController;
                     });
             })
     }
+    static createPaymentCard(req, res) {
+
+        Payment.addPaymentCard(req)
+            .then(result => {
+                res.status(200).send("added payment card");
+            })
+            .catch(err => {
+                res.status(400).send(err);
+            });
+
+    }
+
 }
 
+
 module.exports = PaymentController;
->>>>>>> master
+
