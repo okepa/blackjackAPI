bfe47b0c28d0433d81765a3172dc5a4963567d92
Tech-A42 <Tech-A42@Tech-A42s-MacBook-Air.local>
2017-01-25 10:30:06 +0000

    UNDONE

--- routes/routes.js
+++ routes/routes.js
@@ -15,7 +15,7 @@ router.route("/")
     .get(indexController.showIndex);
 
 //gets registerjs and is implemented through the RegisterController
-router.route("/registration")
+router.route("/register")
     .get(registerController.initUser)
     .post(registerController.makeUser);
 
